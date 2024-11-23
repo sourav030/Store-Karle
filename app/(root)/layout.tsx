291dff31811d53ec6b25499c785af5086e190374
rupesh-dev30 <cgcrupesh@gmail.com>
2024-11-24 00:42:32 +0530

    bug fixing - 3

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,10 +6,12 @@ import { Toaster } from "@/components/ui/toaster";
 import { getCurrentUser } from "@/lib/actions/user.action";
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 const layout = async ({ children }: { children: React.ReactNode }) => {
   const currentUser = await getCurrentUser();
 
-  if(!currentUser) redirect("/sign-in");
+  if(!currentUser) return redirect("/sign-in");
 
   return (
     <main className="flex h-screen">
