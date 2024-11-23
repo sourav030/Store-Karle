0f13dfd80e6df629473de15a5952be98301692a3
rupesh-dev30 <cgcrupesh@gmail.com>
2024-11-23 22:38:42 +0530

    logging session

--- lib/appwrite/index.ts
+++ lib/appwrite/index.ts
@@ -11,7 +11,10 @@ export const createSessionClient = async () => {
 
   const session = (await cookies()).get("appwrite-session");
 
-  if (!session || !session.value) throw new Error("No session");
+  if (!session || !session.value) {
+    console.error("Session cookie is missing or invalid");
+    throw new Error("No session");
+  }
 
   client.setSession(session.value);
 
